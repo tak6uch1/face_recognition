a128e553975b25a1812f7077e64bfade497bffc7
Shinya Takeuchi <tak6uch1@gmail.com>
2021-05-03 11:23:46 +0900

    Update install linux

--- install_linux.sh
+++ install_linux.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-sudo apt -y update
-sudo apt -y upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
-sudo apt -yV install build-essential
-sudo apt -yV install cmake
-sudo apt -yV install libeigen3-dev
-sudo apt -yV install libgtk-3-dev
-sudo apt -yV install qt5-default
-sudo apt -yV install libvtk7-qt-dev
-sudo apt -yV install freeglut3-dev
-sudo apt -yV install libtbb-dev
-sudo apt -yV install libjpeg-dev
-sudo apt -yV install libopenjp2-7-dev
-sudo apt -yV install libpng++-dev
-sudo apt -yV install libtiff-dev
-sudo apt -yV install libopenexr-dev
-sudo apt -yV install libwebp-dev
-sudo apt -yV install libavresample-dev
-sudo apt -yV install libhdf5-dev
-sudo apt -yV install libpython3-dev
-sudo apt -yV install python3-numpy python3-scipy python3-matplotlib
-sudo apt -yV install libboost-all-dev
-sudo apt -yV install git
+sudo apt-get -yV install build-essential
+sudo apt-get -yV install cmake
+sudo apt-get -yV install libeigen3-dev
+sudo apt-get -yV install libgtk-3-dev
+sudo apt-get -yV install qt5-default
+sudo apt-get -yV install libvtk7-qt-dev
+sudo apt-get -yV install freeglut3-dev
+sudo apt-get -yV install libtbb-dev
+sudo apt-get -yV install libjpeg-dev
+sudo apt-get -yV install libopenjp2-7-dev
+sudo apt-get -yV install libpng++-dev
+sudo apt-get -yV install libtiff-dev
+sudo apt-get -yV install libopenexr-dev
+sudo apt-get -yV install libwebp-dev
+sudo apt-get -yV install libavresample-dev
+sudo apt-get -yV install libhdf5-dev
+sudo apt-get -yV install libpython3-dev
+sudo apt-get -yV install python3-numpy python3-scipy python3-matplotlib
+sudo apt-get -yV install libboost-all-dev
+sudo apt-get -yV install git
 
 sudo pip3 install imutils
 
